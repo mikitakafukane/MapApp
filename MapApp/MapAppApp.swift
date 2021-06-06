0ad717b5db31b0f6c403517eeba73d4c746bb311
mikitakafukane <arsene.miyaniii.4@gmail.com>
2021-06-06 22:03:06 +0900

    Initial Commit

---
+++ MapApp/MapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppApp.swift
+//  MapApp
+//
+//  Created by 深根幹貴 on 2021/06/06.
+//
+
+import SwiftUI
+
+@main
+struct MapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
